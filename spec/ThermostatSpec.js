9205e9d223124e02e346a2122d0c9e5a9fb76711
James Forster <jamesalexforster@gmail.com>
2020-07-08 15:51:36 +0100

    can increase or decrease thermostat temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,20 @@ describe ('Thermostat', function() {
 
   });
 
+  describe('up', function() {
+    it('will increase the temp by the argument passed', function () {
+      thermostat = new Thermostat();
+      thermostat.up(1);
+      expect(thermostat.temp).toEqual(21);
+    })
+  })
+
+  describe('down', function() {
+    it('will decrease the temp by the argument passed', function () {
+      thermostat = new Thermostat();
+      thermostat.down(1);
+      expect(thermostat.temp).toEqual(19);
+    })
+  })
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,5 +3,13 @@ class Thermostat {
   constructor() {
     this.temp = 20
   }
-  
+
+  up(n) {
+    this.temp += n
+  }
+
+  down(n) {
+    this.temp -= n
+  }
+
 }
