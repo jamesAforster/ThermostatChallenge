18ae42783851dca7f75d19b44d6a83a37c964379
James Forster <jamesalexforster@gmail.com>
2020-07-09 16:10:53 +0100

    power saving mode works

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -28,7 +28,19 @@ describe ('Thermostat', function() {
       thermostat.down(10);
       expect (function () {thermostat.down(1);}).toThrow(Error('10 is the minimum temp'))
     })
-
   })
 
+  describe('powerMode', function() {
+    it('be turned on when thermostat is created', function () {
+      thermostat = new Thermostat();
+      expect (function () {thermostat.up(20);}).toThrow(Error('25 is the maximum temp'))
+    });
+
+    it('can be turned off and effect the max temp', function () {
+      thermostat = new Thermostat();
+      thermostat.powerMode();
+      expect(thermostat.powersaving).toEqual(false)
+      expect (function () {thermostat.up(20);}).toThrow(Error('32 is the maximum temp'))
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,16 +1,30 @@
 class Thermostat {
 
   constructor() {
-    this.temp = 20
+    this.temp = 20;
+    this.max = 25;
+    this.min = 10;
+    this.powersaving = true;
   }
 
   up(n) {
+    if ((this.temp + n) > this.max) throw Error(`${this.max} is the maximum temp`)
     this.temp += n
   }
 
   down(n) {
-    if ((this.temp - n) < 10) throw Error("10 is the minimum temp");
+    if ((this.temp - n) < this.min) throw Error(`${this.min} is the minimum temp`);
     this.temp -= n
   }
 
+  powerMode() {
+    if (this.powersaving == true) {
+      this.powersaving = false
+      this.max = 32
+    }
+    else {
+      this.powersaving = true
+      this.max = 25
+    }
+  }
 }
